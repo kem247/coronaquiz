88cd693bae1590b7585562fcd94d63563250f852
Kemi Thomas <kemothom71@gmail.com>
2020-05-25 22:41:08 -0400

    Updates

---
+++ precache-manifest.b6064ba8127b6b335284cfb89627a231.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4fd19f916a511ee72e19197ba0899d77",
+    "url": "/coronaquiz/index.html"
+  },
+  {
+    "revision": "3e216f9f8580de8f0065",
+    "url": "/coronaquiz/static/css/main.ca54f056.chunk.css"
+  },
+  {
+    "revision": "fd1f51850667b9257edf",
+    "url": "/coronaquiz/static/js/2.4a199013.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/coronaquiz/static/js/2.4a199013.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3e216f9f8580de8f0065",
+    "url": "/coronaquiz/static/js/main.e9dcaa15.chunk.js"
+  },
+  {
+    "revision": "58e1e89abe617938deaa",
+    "url": "/coronaquiz/static/js/runtime-main.8ed91856.js"
+  }
+]);
